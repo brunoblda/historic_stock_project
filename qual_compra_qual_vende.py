2701dfa5f7527c9484a8af294958b85bd7619f52
brunoblda <brunoblda@gmail.com>
2022-04-09 00:19:56 -0300

    incluindo verficacao qual compra e qual vende

---
+++ qual_compra_qual_vende.py
@@ -0,0 +1,29 @@
+import traceback
+
+def calcula_qual_comprar_e_qual_vender(maior,menor, resultado):
+
+    if resultado == 0:
+        return  'Comprar {} e vender {} !!!'.format(maior, menor)
+    elif resultado == 1:
+        return 'Comprar{} e vender{} !!!'.format(menor, maior)
+    else:
+        return "Erro, entrada de dados inexistente"
+
+if __name__ == '__main__':
+
+    maior = input("Informe a ação maior: ")
+    menor = input("Informe a ação menor: ")
+    print() 
+    try:
+        resultado = int(input("Informe 0 se você espera que a diferença aumente, ou informe 1 se você espera que a diferença diminua: "))
+
+        solucao = calcula_qual_comprar_e_qual_vender(maior,menor,resultado) 
+
+        print(solucao)
+
+    except Exception:
+        print()
+        print(traceback.format_exc()) 
+        print("Aconteceu algum erro exception")
+        print()
+
